863ba1d44e4c8e73eeab8851e001eb8f498f2039
urreita9 <francisco.urrea9@gmail.com>
2022-04-30 20:34:09 -0300

    React initialized

---
+++ client/src/ChatApp.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ChatApp = () => {
+	return <div>ChatApp</div>;
+};
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChatApp } from './ChatApp';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<ChatApp />
+	</React.StrictMode>
+);
