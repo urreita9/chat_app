be453b8efd21f51860a218e4313498a61b089df3
urreita9 <francisco.urrea9@gmail.com>
2022-05-01 19:37:07 -0300

    chatContext, chatReducer

---
+++ client/src/context/chat/ChatContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useReducer } from 'react';
+import { chatReducer } from './chatReducer';
+
+export const ChatContext = createContext();
+
+const initialState = {
+	uid: '',
+	activeChat: null,
+	users: [], // all users
+	messages: [], //selected chat
+};
+
+export const ChatProvider = ({ children }) => {
+	const [chatState, dispatch] = useReducer(chatReducer, initialState);
+
+	return (
+		<ChatContext.Provider value={{ chatState, dispatch }}>
+			{children}
+		</ChatContext.Provider>
+	);
+};
